c99b38ee53f9ba6efccad65c3f09b7980ccd2f1a
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2020-01-04 09:32:59 -0500

    Add program

---
+++ video-upscale.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+##############################################################################
+# Script: video-upscale.pl
+# Author: Glenn Hoeppner
+# License: MIT
+# Repository: https://github.com/StilgarISCA/VideoUpscale
+#
+# Script which makes use of other libraries and executibiles to upscale 
+# videos.
+##############################################################################
