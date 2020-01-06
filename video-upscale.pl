69c6d4f8fe49cb3665be8e02013880db6550a3ba
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2020-01-06 00:14:24 -0500

    Dumps frames from vid

--- video-upscale.pl
+++ video-upscale.pl
@@ -7,13 +7,57 @@
 #
 # Script which makes use of other libraries and executibiles to upscale 
 # videos.
+#
+# https://github.com/imxieyi/waifu2x-mac
 ##############################################################################
+use strict;
+use warnings;
+use POSIX();
 
-# Open file
-# Extract frames
+my $username = getpwuid( $< );
+
+# User-defined parameters
+
+my $FFMPEG = "/usr/local/bin/ffmpeg"; # path to ffmpeg executable
+my $WAIFU2x = ""; # path to waifu2x executable
+
+my $TMP = "";  # path to temporary file storage
+my $SRC = "/Users/$username/upscale/source/test.mp4";  # path to video to upscale
+my $DST = "/Users/$username/upscale/output/";  # path for final video
+
+# No user-defined parameters beyond this point
+
+#
+# Dump frames from a video
+#
+# Accepts:
+# -path to source file
+# -path to destination folder
+#
+sub DumpFrames
+{
+  my ( $src, $dst ) = @_;
+  
+  $dst .= "image%06d.jpg";
+  
+  system( $FFMPEG, "-i", $src , $dst, "-hide_banner" );
+}
+
+### Start Main Program ###
+
+print "Starting Execution\n";
+
+# Test if file exists
+die( "File does not exist or is empty\n $SRC\n" ) if not ( -s $SRC);
+
+DumpFrames( $SRC, $DST );
+
 # Extract audio
 # Upscale frames
 # Sharpen frames
 # Interpolate
 # Reassemble frames
 # Reassemble audio
+
+print "Execution Complete\n";
+exit( 0 );
