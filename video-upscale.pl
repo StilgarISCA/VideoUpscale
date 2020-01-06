a50b6adc760599cad1e40c828a11b1a74703c2be
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2020-01-06 00:32:03 -0500

    Start upscaling with Waifu2x

--- video-upscale.pl
+++ video-upscale.pl
@@ -8,20 +8,21 @@
 # Script which makes use of other libraries and executibiles to upscale 
 # videos.
 #
-# https://github.com/imxieyi/waifu2x-mac
 ##############################################################################
-use strict;
+#use strict;
 use warnings;
 use POSIX();
+
+$| = 1;
 
 my $username = getpwuid( $< );
 
 # User-defined parameters
 
 my $FFMPEG = "/usr/local/bin/ffmpeg"; # path to ffmpeg executable
-my $WAIFU2x = ""; # path to waifu2x executable
+my $WAIFU2X = "/usr/local/bin/waifu2x"; # path to waifu2x executable https://github.com/imxieyi/waifu2x-mac
 
-my $TMP = "";  # path to temporary file storage
+my $TMP = "/Users/$username/upscale/temp/";  # path to temporary file storage
 my $SRC = "/Users/$username/upscale/source/test.mp4";  # path to video to upscale
 my $DST = "/Users/$username/upscale/output/";  # path for final video
 
@@ -41,6 +42,38 @@ sub DumpFrames
   $dst .= "image%06d.jpg";
   
   system( $FFMPEG, "-i", $src , $dst, "-hide_banner" );
+
+  return;
+}
+
+#
+# Upscales all images in a folder
+#
+# Accepts:
+# -path to source folder
+# -path to destination folder
+#
+sub UpscaleFrames
+{
+  my ( $src, $dst ) = @_;
+
+  opendir( DIR, $src ) or die( "Could not open folder\n" );
+  while( readdir( DIR ) ){
+  	
+  	next if (-d -z $_ );
+  	
+  	print "file: $_\n";
+  	
+  	local ($name, $ext) = split( /\./, $_ );
+  	
+  	local $cur_file_path = $src . $_;
+  	local $dst_file_path = $dst . $name . ".png";
+  	  
+    system( "$WAIFU2X -t p -s 2 -n 4 -i $cur_file_path -o $dst_file_path" );
+  }
+  closedir( DIR );
+  
+  return;
 }
 
 ### Start Main Program ###
@@ -50,10 +83,12 @@ print "Starting Execution\n";
 # Test if file exists
 die( "File does not exist or is empty\n $SRC\n" ) if not ( -s $SRC);
 
-DumpFrames( $SRC, $DST );
+DumpFrames( $SRC, $TMP );
 
 # Extract audio
-# Upscale frames
+
+UpscaleFrames( $TMP, $DST );
+
 # Sharpen frames
 # Interpolate
 # Reassemble frames
